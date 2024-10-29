cf5d7bcbabbcaef3e5743b46da0dc65bf1d17abc
hulutech-web <yuanhaozhuzhu@hotmail.com>
2024-10-29 09:48:39 +0800

    数据返回为nil时处理

--- http_result.go
+++ http_result.go
@@ -41,6 +41,10 @@ func (h *HttpResult) Success(message string, data interface{}) http.Response {
 	if message == "" {
 		message = facades.Config().GetString("http_result.Message")
 	}
+	//查询的结果有可能存在data为nil的情况，判断如果是nil则，给定一个[]string{}类型的数据，避免前端页面报错
+	if data == nil {
+		data = []string{}
+	}
 	return h.Context.Response().Success().Json(http.Json{
 		"message": message,
 		"data":    data,
